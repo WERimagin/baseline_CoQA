6fecb5128cb632c3b24fe349a13bd2b436c64f2d
笹沢裕一 <werimagin@gmail.com>
2018-12-26 14:20:30 +0900

    add

--- scripts/gen_pipeline_data.py
+++ scripts/gen_pipeline_data.py
@@ -68,6 +68,9 @@ def process(text):
         end=start+len(t)
         output['word'].append(_str(t))
         output['offsets'].append((start, end))
+        print(start,end)
+    print(len(text))
+    print()
     return output
 
 
