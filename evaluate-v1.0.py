a5416ef236c49655bc9cf3acf79d291c02b0c652
笹沢裕一 <werimagin@gmail.com>
2019-04-23 10:34:02 +0900

    add

--- evaluate-v1.0.py
+++ evaluate-v1.0.py
@@ -49,6 +49,7 @@ class CoQAEvaluator():
                 if story["answers"][i]["span_start"]==-1:
                     continue
                 if story["answers"][i]["input_text"]=="yes" or story["answers"][i]["input_text"]=="no":
+                    continue
                 gold_answers = []
                 for j,answers in enumerate(multiple_answers):
                     if j==0:answer=answers[i]
