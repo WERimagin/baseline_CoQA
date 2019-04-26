f3d7e2570c5904f94983f60e0be8f293b2cb6dfc
笹沢裕一 <werimagin@gmail.com>
2019-04-26 15:16:18 +0900

    add

--- predict.sh
+++ predict.sh
@@ -9,7 +9,7 @@ python rc/main.py \
 --testset data/coqa.dev.pipeline.json \
 --n_history 0 \
 --pretrained pipeline_models --pretrained_model pipeline_models/params-interro-beam2-0425.saved \
---cuda_id 3
+--cuda_id $2
 
 python scripts/gen_pipeline_for_seq2seq.py \
 --data_file data/coqa.dev.pipeline.json \
@@ -20,7 +20,7 @@ python seq2seq/translate.py \
 -model pipeline_models/seq2seq_copy_acc_84.71_ppl_2.18_e25.pt \
 -src pipeline_models/pipeline-seq2seq-src.txt \
 -output pipeline_models/pred.txt \
--replace_unk -dynamic_dict -gpu 3
+-replace_unk -dynamic_dict -gpu $2
 
 python scripts/gen_seq2seq_output.py \
 --data_file data/coqa-dev-$1.json \
