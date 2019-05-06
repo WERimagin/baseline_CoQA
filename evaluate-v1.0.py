1d36749cbc449a8ec933d9fe527da0254772fb47
笹沢裕一 <werimagin@gmail.com>
2019-05-06 20:43:46 +0900

    add

--- evaluate-v1.0.py
+++ evaluate-v1.0.py
@@ -51,12 +51,13 @@ class CoQAEvaluator():
 
                 gold_answers = []
                 for j,answers in enumerate(multiple_answers):
+                    qid-=1
                     #print(qid,len(answers))
                     if qid<len(answers):
-                        answer=answers[qid-1]
+                        answer=answers[qid]
                     #modify_question
                     else:
-                        answer=answers[qid-1-len(answers)]
+                        answer=answers[qid-len(answers)]
 
                     """
                     if j==0:
